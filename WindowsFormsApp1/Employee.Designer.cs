0890d38d2c705370d547eeb5a63d84e39070df27
chiub2 <chiub2@mymacewan.ca>
2022-03-08 12:08:26 -0700

    New Changes to size of app

--- WindowsFormsApp1/Employee.Designer.cs
+++ WindowsFormsApp1/Employee.Designer.cs
@@ -42,10 +42,9 @@
             // CustomerBtn
             // 
             this.CustomerBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CustomerBtn.Location = new System.Drawing.Point(687, 13);
-            this.CustomerBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.CustomerBtn.Location = new System.Drawing.Point(1083, 51);
             this.CustomerBtn.Name = "CustomerBtn";
-            this.CustomerBtn.Size = new System.Drawing.Size(100, 40);
+            this.CustomerBtn.Size = new System.Drawing.Size(75, 32);
             this.CustomerBtn.TabIndex = 2;
             this.CustomerBtn.Text = "Logout";
             this.CustomerBtn.UseVisualStyleBackColor = true;
@@ -57,57 +56,63 @@
             this.Tabs.Controls.Add(this.tabPage2);
             this.Tabs.Controls.Add(this.tabPage1);
             this.Tabs.Controls.Add(this.tabPage3);
-            this.Tabs.Location = new System.Drawing.Point(13, 13);
+            this.Tabs.Location = new System.Drawing.Point(10, 11);
+            this.Tabs.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Tabs.Name = "Tabs";
             this.Tabs.SelectedIndex = 0;
-            this.Tabs.Size = new System.Drawing.Size(667, 425);
+            this.Tabs.Size = new System.Drawing.Size(1058, 612);
             this.Tabs.TabIndex = 3;
             // 
             // carsTab
             // 
-            this.carsTab.Location = new System.Drawing.Point(4, 25);
+            this.carsTab.Location = new System.Drawing.Point(4, 22);
+            this.carsTab.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.carsTab.Name = "carsTab";
-            this.carsTab.Padding = new System.Windows.Forms.Padding(3);
-            this.carsTab.Size = new System.Drawing.Size(659, 396);
+            this.carsTab.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.carsTab.Size = new System.Drawing.Size(1050, 586);
             this.carsTab.TabIndex = 0;
             this.carsTab.Text = "Cars";
             this.carsTab.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(659, 396);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(1048, 403);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Car Type";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(659, 396);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(1048, 403);
             this.tabPage1.TabIndex = 2;
             this.tabPage1.Text = "Branches";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(659, 396);
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage3.Size = new System.Drawing.Size(1048, 403);
             this.tabPage3.TabIndex = 3;
             this.tabPage3.Text = "Customers";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(688, 370);
+            this.button1.Location = new System.Drawing.Point(1083, 490);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 64);
+            this.button1.Size = new System.Drawing.Size(75, 52);
             this.button1.TabIndex = 4;
             this.button1.Text = "Report";
             this.button1.UseVisualStyleBackColor = true;
@@ -115,22 +120,24 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(688, 175);
+            this.button2.Location = new System.Drawing.Point(1083, 262);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 64);
+            this.button2.Size = new System.Drawing.Size(75, 52);
             this.button2.TabIndex = 5;
             this.button2.Text = "Rent";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Employee
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1180, 647);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Tabs);
             this.Controls.Add(this.CustomerBtn);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Employee";
             this.Text = "Form1";
             this.Tabs.ResumeLayout(false);
